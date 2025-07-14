6e8fa623063a06dd675e237b01af2840c8dc559a
manoj-5656 <kathimanojreddy@gmail.com>
2025-07-14 20:40:35 +0530

    lmsbackend

--- server.py
+++ server.py
@@ -1,27 +1,22 @@
-from flask import Flask,request,jsonify,render_template
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from config.database import connect_db
 from routes.authRoutes import auth_bp
 from routes.courseRoute import course_bp
+import os
 
-app=Flask(__name__)
-CORS(app,supports_credentials=True,origins="http://localhost:5173")
+app = Flask(__name__)
+CORS(app, supports_credentials=True, origins="https://lms-jawn.vercel.app")
 connect_db()
 
-@app.route('/',methods=['GET'])
+@app.route('/', methods=['GET'])
 def home():
-    return "this is from local host 4000"
+    return "this is from deployed backend"
 
-app.register_blueprint(auth_bp,url_prefix='/api/auth')
-app.register_blueprint(course_bp,url_prefix='/api/course')
+# Register Blueprints
+app.register_blueprint(auth_bp, url_prefix='/api/auth')
+app.register_blueprint(course_bp, url_prefix='/api/course')
 
-
-
-
-
-    
-   
-
-
-if __name__=="__main__":
-    app.run(host="localhost",port=4000,debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use environment PORT or default to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
